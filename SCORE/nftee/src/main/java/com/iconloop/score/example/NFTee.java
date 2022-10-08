a33a64c5cb3e356b92bdb1ec64c09b96e3a16e12
SnowyField1906 <aswdqe1x@gmail.com>
2022-10-09 00:39:56 +0700

    add SCORE folder

---
+++ SCORE/nftee/src/main/java/com/iconloop/score/example/NFTee.java
@@ -0,0 +1,47 @@
+package com.iconloop.score.example;
+
+import score.Address;
+import score.Context;
+import score.ArrayDB;
+import score.BranchDB;
+import score.annotation.External;
+
+import scorex.util.ArrayList;
+
+import java.util.List;
+
+public class NFTee {
+    private String title;
+    private String description;
+
+    public NFTee(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    BranchDB<String, ArrayList<Address>> proposals = Context.newBranchDB("proposals", Address.class);
+
+    private boolean avaiableVotes(String vote) {
+        return vote.equals("For") || vote.equals("Against") || vote.equals("Abstain");
+    }
+
+    @External()
+    public void Vote(String proposal) {
+        Context.require(!proposals.at(proposal).contains(Context.getCaller()));
+        Context.require(avaiableVotes(proposal));
+        proposals.at(proposal).add(Context.getCaller());
+    }
+
+    @External(readonly = true)
+    public int getVoteCount(String proposal) {
+        Context.require(avaiableVotes(proposal));
+        return proposals.at(proposal).size();
+    }
+
+    @External(readonly = true)
+    public List<Address> getVoters(String proposal) {
+        Context.require(avaiableVotes(proposal));
+        return proposals.at(proposal);
+    }
+
+}
