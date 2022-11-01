9f47aafdcc055b1eaf91393f4b21027ab332890e
SnowyField1906 <aswdqe1x@gmail.com>
2022-11-01 16:04:11 +0700

    ready for showcase

--- src/pages/Logs/index.js
+++ src/pages/Logs/index.js
@@ -39,7 +39,7 @@ function Logs({ account }) {
         <div className='w-screen h-screen fixed -z-10 backdrop-blur-md bg-gray-200/30 dark:bg-gray-800/30 overflow-x-hidden'>
 
         </div>
-        <div className="content-board-view pt-36">
+        <div className="content-board-view pt-32">
           {
             notifications.slice(20 * page, 20 * page + 20).map((notification) => {
               return (
@@ -50,11 +50,11 @@ function Logs({ account }) {
             })
           }
         </div>
-        <div className='flex absolute bottom-5 z-20 '>
-          <button className={`${page === 0 ? 'button-medium-disabled cursor-not-allowed' : 'button-medium cursor-pointer'} w-32 text-center text-medium text-xl h-fit py-3 px-5 rounded-md`} disabled={page <= 0}
+        <div className='flex absolute bottom-3 z-20 bg-white/20 dark:bg-black/20 py-3 px-5 rounded-xl backdrop-lg'>
+          <button className={`${page === 0 ? 'button-medium-disabled cursor-not-allowed' : 'button-medium cursor-pointer'} w-32 text-center text-medium text-lg h-fit py-2 px-5 rounded-md`} disabled={page <= 0}
             onClick={() => setPage(page - 1)}>Previous</button>
-          <div className='text-medium text-xl h-fit py-3 px-5 rounded-md'>Page {page + 1} ({20 * page + 20 < notifications.length ? 20 * page + 20 : notifications.length}/{notifications.length})</div>
-          <button className={`${20 * page + 20 > notifications.length ? 'button-medium-disabled cursor-not-allowed' : 'button-medium cursor-pointer'} w-32 text-center text-medium text-xl h-fit py-3 px-5 rounded-md`} disabled={20 * page + 20 > notifications.length}
+          <div className='text-medium text-lg h-fit py-2 px-5 rounded-md'>Page {page + 1} ({20 * page + 20 < notifications.length ? 20 * page + 20 : notifications.length}/{notifications.length})</div>
+          <button className={`${20 * page + 20 > notifications.length ? 'button-medium-disabled cursor-not-allowed' : 'button-medium cursor-pointer'} w-32 text-center text-medium text-lg h-fit py-2 px-5 rounded-md`} disabled={20 * page + 20 > notifications.length}
             onClick={() => setPage(page + 1)}>Next</button>
         </div>
       </div>
